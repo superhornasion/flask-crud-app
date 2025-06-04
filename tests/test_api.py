4a1e71123ef2cbfb5174b2be4d0e11a6d1b4304d
superhornasion <juanruizo777@gmail.com>
2025-06-03 19:18:49 -0500

    Cambiar pruebas unitarias

--- tests/test_api.py
+++ tests/test_api.py
@@ -1,16 +1,26 @@
-from api import app
+# tests/test_api.py
 import pytest
+import os
+from unittest.mock import patch, MagicMock
+
+# Mockea la variable de entorno DB_CONNECTION_STRING
+with patch.dict(os.environ, {'DB_CONNECTION_STRING': 'mock_connection_string'}):
+    from api import app, get_db_connection
 
 
 @pytest.fixture
 def client():
-    app.config['TESTING'] = True  # Activa el modo de prueba de Flask
+    app.config['TESTING'] = True
     with app.test_client() as client:
-        yield client  # Retorna el cliente de prueba para que los tests lo usen
+        yield client
 
 
-def test_home_page(client):
-    # Hace una solicitud GET a la ruta raíz
+@patch('api.get_db_connection')
+def test_home_page(mock_get_db_connection, client):
+    mock_conn = MagicMock()
+    mock_get_db_connection.return_value = mock_conn
+    
     response = client.get('/')
     assert response.status_code == 200
-    assert b"Hola mundo" in response.data
+    assert b"Hola, mundo" in response.data
+    mock_get_db_connection.assert_called_once()
